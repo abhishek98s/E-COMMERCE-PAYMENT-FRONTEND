0d88a5fd5d2969109d15823a4b76c6f18f96a193
abhsihek <abhishekshakya98.com@gmail.com>
2025-07-17 09:31:46 +0545

    feat: pagination

---
+++ src/features/pagination/index.tsx
@@ -0,0 +1,61 @@
+import { Dispatch, SetStateAction } from 'react';
+
+type PaginationProps = {
+  setCurrentPage: Dispatch<SetStateAction<number>>;
+  totalPage: number;
+  currentPage: number;
+};
+
+const Pagination = ({
+  totalPage,
+  currentPage,
+  setCurrentPage,
+}: PaginationProps) => {
+  const pages = [...Array(totalPage)].map((a, i) => i + 1);
+
+  const onBtnClick = (num: number) => {
+    if (
+      (num < 0 && currentPage === 1) ||
+      (num > 0 && currentPage === totalPage)
+    ) {
+      return;
+    }
+    setCurrentPage((prev) => prev + num);
+  };
+
+  return (
+    <div className='pagination-wrapper mx-auto flex-center gap-[12px]'>
+      <button
+        onClick={() => onBtnClick(-1)}
+        disabled={currentPage === 1 ? true : false}
+        className={`${currentPage === 1 ? 'opacity-20' : ''} prev flex-center rounded-[8px] px-3 min-h-[44px] bg-neutral-0 border-neutral-200`}
+      >
+        Previous
+      </button>
+
+      <div className='flex gap-[8px] flex-center'>
+        {totalPage &&
+          pages.map((page: number) => (
+            <a
+              href='#'
+              onClick={() => setCurrentPage(page)}
+              key={page}
+              className={`${currentPage === page ? 'font-bold size-[48px]' : ''} size-[44px] flex-center text-[16px] bg-neutral-0 border-neutral-200 rounded-[8px] shadow-sm `}
+            >
+              {page}
+            </a>
+          ))}
+      </div>
+
+      <button
+        onClick={() => onBtnClick(1)}
+        disabled={currentPage === totalPage ? true : false}
+        className={`${currentPage === totalPage ? 'opacity-20' : ''} next flex-center rounded-[8px] px-3 min-h-[44px] bg-neutral-0 border-neutral-200`}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
