f9deb4a9dc70088adcdeb70c94509a5c7be35d8e
abhsihek <abhishekshakya98.com@gmail.com>
2025-07-17 10:19:23 +0545

    add: useFetch hook and axios for data fetching

---
+++ src/shared/hooks/use-fetch.hooks.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { axiosInterceptor } from '../utils/axois.config';
+import { APP_BASE_URL } from '../utils/app';
+import axios from 'axios';
+
+export const useFetch = <T,>(url: string, options = {}) => {
+  const [data, setData] = useState<T[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      const axiosInstance = axiosInterceptor();
+
+      try {
+        const response = await axiosInstance.get(`${APP_BASE_URL}/${url}`, {
+          ...options,
+        });
+
+        setData(response.data);
+      } catch (error) {
+        const err = (error as Error).message;
+
+        if (axios.isCancel(err)) {
+          console.log('Request canceled:', err.message);
+        } else {
+          setError(err);
+        }
+        console.log('useFetch error');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
