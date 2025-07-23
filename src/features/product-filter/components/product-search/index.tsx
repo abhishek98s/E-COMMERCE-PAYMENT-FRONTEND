4f8d203b1b0464861f5c25c44f3ba1e3e1be7be5
abhishek98s <abhishekshakya98.com@gmail.com>
2025-07-23 21:33:03 +0545

    fix: input search empty on oustide click

--- src/features/product-filter/components/product-search/index.tsx
+++ src/features/product-filter/components/product-search/index.tsx
@@ -1,5 +1,5 @@
 import ImageWrapper from '@/shared/components/img-wrapper/img-wrapper.component';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 
 type ProductSearchProps = {
   setSearchTitle: (title: string | null) => void;
@@ -8,41 +8,43 @@ type ProductSearchProps = {
 const ProductSearch = ({ setSearchTitle }: ProductSearchProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        inputRef.current &&
-        inputRef.current.value.length > 0 &&
-        !inputRef.current.contains(event.target as Node)
-      ) {
-        inputRef.current.value = '';
-        setSearchTitle(null);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
+  const handleResetBtn = () => {
+    if (inputRef.current) {
+      inputRef.current.value = '';
+      setSearchTitle(null);
+    }
+  };
 
   return (
-    <div className='search-wrapper relative overflow-hidden rounded-[8px] max-w-[500px] w-full'>
+    <div className="search-wrapper relative overflow-hidden rounded-[8px] max-w-[500px] w-full">
       <input
         ref={inputRef}
         onChange={(e) => setSearchTitle(e.target.value)}
-        type='text'
-        placeholder='Search for a product'
-        className='search-input p-[12px] pl-[48px]'
+        type="text"
+        placeholder="Search for a product"
+        className="search-input p-[12px] pl-[48px]"
       />
-      <div className='absolute top-1/2 left-0 -translate-y-1/2'>
-        <button className='search-btn size-[48px] flex-center '>
+      <div className="absolute top-1/2 left-0 -translate-y-1/2">
+        <button className="search-btn size-[48px] flex-center ">
           <ImageWrapper
-            className='max-w-[24px] opacity-50'
-            path='/icons/search.svg'
+            className="max-w-[24px] opacity-50"
+            path="/icons/search.svg"
           />
         </button>
       </div>
+      {inputRef.current && inputRef.current.value.length > 0 && (
+        <div className="absolute top-1/2 right-0 -translate-y-1/2">
+          <button
+            onClick={handleResetBtn}
+            className="search-btn size-[48px] flex-center "
+          >
+            <ImageWrapper
+              className="max-w-[24px] opacity-50"
+              path="/icons/close.svg"
+            />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
